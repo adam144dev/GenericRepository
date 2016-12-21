018ffb330dd7e2f7e072b6e7d44429ac530fb4fb
adam144dev <adam144dev@gmail.com>
2016-12-21 23:17:29 +0100

    initial

---
+++ GenericRepository/src/GenericRepository/EFBaseRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace GenericRepository
+{
+    public class EFBaseRepository<TEntity> : IBaseRepository<TEntity>
+        where TEntity : class
+    {
+        protected readonly DbContext _dbContext;
+
+        public EFBaseRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IQueryable<TEntity> Entities => _dbContext.Set<TEntity>();
+
+        public virtual IQueryable<TEntity> Include(params string[] paths)
+        {
+            IQueryable<TEntity> dbQuery = _dbContext.Set<TEntity>();
+
+            foreach (string path in paths)
+                dbQuery = dbQuery.Include(path);
+
+            return dbQuery;
+        }
+
+
+        public void Add(TEntity entity)
+        {
+            _dbContext.Add(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbContext.Entry<TEntity>(entity).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _dbContext.Remove(entity);
+            _dbContext.SaveChanges();
+        }
+    }
+}
---
+++ GenericRepository/src/GenericRepository/IBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace GenericRepository
+{
+    public interface IBaseRepository<TEntity>
+        where TEntity : class
+    {
+        IQueryable<TEntity> Entities { get; }
+
+        IQueryable<TEntity> Include(params string[] paths);
+
+
+        void Add(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(TEntity entity);
+    }
+}
