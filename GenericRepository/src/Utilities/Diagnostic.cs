7a2b4d54f2c4035e9260b83cfd5218411c1f8f1a
adam144dev <adam144dev@gmail.com>
2017-01-04 20:23:15 +0100

    Diagnostic - Assert

---
+++ GenericRepository/src/Utilities/Diagnostic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Utilities
+{
+    public static class Diagnostic
+    {
+        public static void Assert(bool condition, string message = null, string detailMessage = null)
+            => Debug.Assert(condition, message ?? string.Empty, detailMessage ?? string.Empty);
+
+
+        public static void Assert<TException>(bool condition, string message, string detailMessage, TException exception)
+            where TException : Exception
+        {
+            Assert(condition, message, detailMessage);  // for DEBUG
+            throw exception;                            // no DEBUG
+        }
+
+        public static void Assert<TException>(bool condition, string message, TException exception)
+            where TException : Exception
+            => Assert<TException>(condition, message, string.Empty, exception);
+
+        public static void Assert<TException>(bool condition, TException exception)
+            where TException : Exception
+            => Assert<TException>(condition, string.Empty, string.Empty, exception);
+
+
+        public static void Assert<TException>(bool condition)
+            where TException : Exception, new()
+        {
+            Debug.Assert(condition);
+            throw new TException();
+        }
+    }
+}
