f8be492a1d258b059120975c29ebaa0e9096012a
adam144dev <adam144dev@gmail.com>
2017-01-04 21:33:25 +0100

    NotNull, NotEmpty

--- GenericRepository/src/Utilities/Diagnostic.cs
+++ GenericRepository/src/Utilities/Diagnostic.cs
@@ -1,30 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Utilities
 {
     public static class Diagnostic
     {
-        public static void Assert(bool condition, string message = null, string detailMessage = null)
+        public static void NotNull(object value, string paramName = null, string message = null)
+        {
+            Assert(
+                value != null,
+                paramName,
+                message,
+                new ArgumentNullException(paramName ?? string.Empty, message ?? string.Empty)
+            );
+        }
+
+        public static void NotEmpty<T>(IEnumerable<T> value, string paramName = null, string message = null)
+        {
+            Assert(
+                value.Any(),
+                paramName,
+                message,
+                new ArgumentException(message ?? string.Empty, paramName ?? string.Empty)
+            );
+        }
+
+
+        //Diagnostic.Assert(false, "message", "detailMessage");
+        public static void Assert(bool condition, string message, string detailMessage)
             => Debug.Assert(condition, message ?? string.Empty, detailMessage ?? string.Empty);
 
+        //Diagnostic.Assert(false, "message");
+        //Diagnostic.Assert(false);
+        public static void Assert(bool condition, string message = null)
+            => Debug.Assert(condition, message ?? string.Empty, string.Empty);
 
+        //Diagnostic.Assert(false, "message", "detailMessage", new XException("message", "detailMessage"));
         public static void Assert<TException>(bool condition, string message, string detailMessage, TException exception)
             where TException : Exception
         {
-            Assert(condition, message, detailMessage);  // for DEBUG
-            throw exception;                            // no DEBUG
+            Assert(condition, message, detailMessage);  // while DEBUG
+            throw exception;                            // always until ^Assert=>Debug.Assert breaks
         }
 
+        //Diagnostic.Assert(false, "message", new XException("message"));
         public static void Assert<TException>(bool condition, string message, TException exception)
             where TException : Exception
             => Assert<TException>(condition, message, string.Empty, exception);
 
+        //Diagnostic.Assert(false, new XException());
         public static void Assert<TException>(bool condition, TException exception)
             where TException : Exception
             => Assert<TException>(condition, string.Empty, string.Empty, exception);
 
 
+        //Diagnostic.Assert<XException>(false);        
         public static void Assert<TException>(bool condition)
             where TException : Exception, new()
         {
